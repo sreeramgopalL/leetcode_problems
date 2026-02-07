2c0fd5903d83e258f6587d4aff64a7189691373a
sreeramgopalL <717823s151@kce.ac.in>
2026-02-07 12:30:28 +0530

    [1] [Time Beats: 27.55%] [Memory Beats: 52.57%] - LeetPush

---
+++ Two-Sum.java
@@ -0,0 +1,14 @@
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        for(int i=0;i<nums.length;i++){
+4            for(int j=i+1;j<nums.length;j++){
+5                if(nums[i]+nums[j]==target){
+6                    return new int[]{i,j};
+7                }
+8            }
+9            
+10        }
+11        return new int[]{};
+12    }
+13    
+14}
